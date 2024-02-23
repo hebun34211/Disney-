bd79c90a5472079ed11c0d6eda7596171b18ccc3
Yukki21090 <158785327+Yukki21090@users.noreply.github.com>
2024-02-23 03:15:05 +0300

    Update callback.py

--- ArchMusic/plugins/admins/callback.py
+++ ArchMusic/plugins/admins/callback.py
@@ -275,7 +275,7 @@ async def del_back_playlist(client, CallbackQuery, _):
                     _["call_9"]
                 )
             button = telegram_markup(_, chat_id)
-            img = await gen_thumb(videoid)
+            img = None
             run = await CallbackQuery.message.reply_text(
                     text=_["stream_1"].format(
                       title,
@@ -308,7 +308,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             except Exception:
                 return await mystic.edit_text(_["call_9"])
             button = stream_markup(_, videoid, chat_id)
-            img = await gen_thumb(videoid)
+            img = None
             run = await CallbackQuery.message.reply_text(
                     text=_["stream_1"].format(
                       title,
@@ -373,7 +373,7 @@ async def del_back_playlist(client, CallbackQuery, _):
                 db[chat_id][0]["markup"] = "tg"
             else:
                 button = stream_markup(_, videoid, chat_id)
-                img = await gen_thumb(videoid)
+                img = None
                 run = await CallbackQuery.message.reply_text(
                     text=_["stream_1"].format(
                       title,
