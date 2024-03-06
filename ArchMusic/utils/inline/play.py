08e481af762e82304af5ddaee7f9368700fe94fb
Yukki21090 <158785327+Yukki21090@users.noreply.github.com>
2024-03-06 04:51:45 +0300

    Update play.py

--- ArchMusic/utils/inline/play.py
+++ ArchMusic/utils/inline/play.py
@@ -35,55 +35,32 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     bar += line*(10-len(bar))
 
     buttons = [
-        
-        
-        [
-            InlineKeyboardButton(
-                text="▷",
-                callback_data=f"ADMIN Resume|{chat_id}",
-            ),
-            InlineKeyboardButton(
-                text="II", callback_data=f"ADMIN Pause|{chat_id}"
-            ),
-            
-            InlineKeyboardButton(
-                text="‣‣", callback_data=f"ADMIN Skip|{chat_id}"
-            ),
-            InlineKeyboardButton(
-                text="▢", callback_data=f"ADMIN Stop|{chat_id}"
-            ), 
-        ],
-        [
+         [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
-        [
-        InlineKeyboardButton(
-                text="✚ ᴘᴀʀᴄ̧ᴀʏɪ ᴛᴇᴋʀᴀʀʟᴀ ✚", callback_data=f"ADMIN Loop|{chat_id}"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="◂◂ 10 ",
-                callback_data=f"ADMIN 1|{chat_id}",
-            ),
-            InlineKeyboardButton(
-                text="◂◂ 30 ",
-                callback_data=f"ADMIN 3|{chat_id}",
-            ),
-            InlineKeyboardButton(
-                text="10 ▸▸",
-                callback_data=f"ADMIN 2|{chat_id}",
-            ),
-            InlineKeyboardButton(
-                text="30 ▸▸",
-                callback_data=f"ADMIN 4|{chat_id}",
-            ),
-        ],
         
-    ]
+        [
+              InlineKeyboardButton(text="✚ ᴘᴀʀᴄ̧ᴀʏɪ ᴛᴇᴋʀᴀʀʟᴀ ✚", callback_data=f"ADMIN Loop|{chat_id}"),
+        ],                 
+        [
+            InlineKeyboardButton(text="▷",callback_data=f"ADMIN Resume|{chat_id}",),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="‣‣", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+             
+        ],
+        [
+           InlineKeyboardButton(text="◂◂ 10 ",callback_data=f"ADMIN 1|{chat_id}",),
+            InlineKeyboardButton(text="◂◂ 30 ",callback_data=f"ADMIN 3|{chat_id}",),
+            InlineKeyboardButton(text="10 ▸▸",callback_data=f"ADMIN 2|{chat_id}",),
+            InlineKeyboardButton(text="30 ▸▸",callback_data=f"ADMIN 4|{chat_id}",),
+        ],
+       
+    ]     
+         
     return buttons
 
 
@@ -126,47 +103,23 @@ def telegram_markup_timer(_, chat_id, played, dur):
 
 def stream_markup(_, videoid, chat_id):
     buttons = [
-        
-        
         [
-            InlineKeyboardButton(
-                text="▷",
-                callback_data=f"ADMIN Resume|{chat_id}",
-            ),
-            InlineKeyboardButton(
-                text="II", callback_data=f"ADMIN Pause|{chat_id}"
-            ),
-            
-            InlineKeyboardButton(
-                text="‣‣", callback_data=f"ADMIN Skip|{chat_id}"
-            ),
-            InlineKeyboardButton(
-                text="▢", callback_data=f"ADMIN Stop|{chat_id}"
-            ), 
+        InlineKeyboardButton(text="✚ ᴘᴀʀᴄ̧ᴀʏɪ ᴛᴇᴋʀᴀʀʟᴀ ✚", callback_data=f"ADMIN Loop|{chat_id}"),
+        ],                 
+        [
+            InlineKeyboardButton(text="▷",callback_data=f"ADMIN Resume|{chat_id}",),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="‣‣", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+             
         ],
         [
-        InlineKeyboardButton(
-                text="✚ ᴘᴀʀᴄ̧ᴀʏɪ ᴛᴇᴋʀᴀʀʟᴀ ✚", callback_data=f"ADMIN Loop|{chat_id}"
-            ),
+           InlineKeyboardButton(text="◂◂ 10 ",callback_data=f"ADMIN 1|{chat_id}",),
+            InlineKeyboardButton(text="◂◂ 30 ",callback_data=f"ADMIN 3|{chat_id}",),
+            InlineKeyboardButton(text="10 ▸▸",callback_data=f"ADMIN 2|{chat_id}",),
+            InlineKeyboardButton(text="30 ▸▸",callback_data=f"ADMIN 4|{chat_id}",),
         ],
-        [
-            InlineKeyboardButton(
-                text="◂◂ 10 ",
-                callback_data=f"ADMIN 1|{chat_id}",
-            ),
-            InlineKeyboardButton(
-                text="◂◂ 30 ",
-                callback_data=f"ADMIN 3|{chat_id}",
-            ),
-            InlineKeyboardButton(
-                text="10 ▸▸",
-                callback_data=f"ADMIN 2|{chat_id}",
-            ),
-            InlineKeyboardButton(
-                text="30 ▸▸",
-                callback_data=f"ADMIN 4|{chat_id}",
-            ),
-        ],
+
     ]
     return buttons
 
