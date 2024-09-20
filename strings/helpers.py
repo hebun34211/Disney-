3354397954db2352dbaa1832e3e9842eb28c4e93
Ali <104919540+olurunabirak@users.noreply.github.com>
2024-09-20 13:01:52 +0300

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -2,7 +2,7 @@
 
 HELP_1 = """✅**<u>BURDAKİ KOMUTLARI YALNIZCA GRUP ADMİNLERİ KULLANABİLİR:</u>**
 ╔═════════════════════════
-║╰☞**Burdaki Komutları Normal Üyelerin De Kullanabilmesi İçin İstediğiniz Kişinin Mesajını Yanıtlayarak /yetkilendir Yazmanız Yeterlidir.**
+║╰☞**Burdaki Komutları Normal Üyelerin ║De Kullanabilmesi İçin İstediğiniz Kişinin ║Mesajını Yanıtlayarak /yetkilendir Yazmanız ║Yeterlidir.**
 ║
 ║ ╰☞**/duraklat :** Müziği Duraklatır.
 ║ ╰☞**/devam :** Müziği Devam Ettirir.
