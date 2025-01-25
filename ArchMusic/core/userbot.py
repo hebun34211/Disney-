bf43b30a5515a41fe7b9f8acf24362720293dae9
Ali <104919540+olurunabirak@users.noreply.github.com>
2025-01-25 15:47:20 +0300

    Update userbot.py

--- ArchMusic/core/userbot.py
+++ ArchMusic/core/userbot.py
@@ -63,9 +63,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("ARCH_SUPPORTS")
-                await self.one.join_chat("ARCH_SUPPORTS")
-                await self.one.join_chat("ARCH_SUPPORTS")
+        
+                await self.one.join_chat("")
             except:
                 pass
             assistants.append(1)
@@ -94,8 +93,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("ARCH_SUPPORTS")
-                await self.two.join_chat("ARCH_SUPPORTS")
+                
                 await self.two.join_chat("ARCH_SUPPORTS")
             except:
                 pass
@@ -125,8 +123,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("ARCH_SUPPORTS")
-                await self.three.join_chat("ARCH_SUPPORTS")
+                
                 await self.three.join_chat("ARCH_SUPPORTS")
             except:
                 pass
@@ -156,8 +153,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("ARCH_SUPPORTS")
-                await self.four.join_chat("ARCH_SUPPORTS")
+                
                 await self.four.join_chat("ARCH_SUPPORTS")
             except:
                 pass
@@ -187,8 +183,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("ARCH_SUPPORTS")
-                await self.five.join_chat("ARCH_SUPPORTS")
+                
                 await self.five.join_chat("ARCH_SUPPORTS")
             except:
                 pass
