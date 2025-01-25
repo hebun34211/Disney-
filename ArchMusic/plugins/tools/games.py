2274e5842ef08711c9209161d98d22096543c8ec
Ali <104919540+olurunabirak@users.noreply.github.com>
2025-01-26 02:19:12 +0300

    Update games.py

--- ArchMusic/plugins/tools/games.py
+++ ArchMusic/plugins/tools/games.py
@@ -1,37 +1,4 @@
-# -*- coding: utf-8 -*-
 
-# (c) @aylak-github (Github) | https://t.me/ayIak | @BasicBots (Telegram)
-
-# ==============================================================================
-#
-# Project: CallToneBot
-# Copyright (C) 2021-2022 by aylak-github@Github, < https://github.com/aylak-github >.
-#
-# This file is part of < https://github.com/aylak-github/CallTone > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/aylak-github/CallTone/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-# ==============================================================================
-#
-# Proje: CallToneBot
-# Telif Hakkı (C) 2021-2022 aylak-Github@Github, <https://github.com/aylak-github>.
-#
-# Bu dosya <https://github.com/aylak-github/CallTone> projesinin bir parçası,
-# ve "GNU V3.0 Lisans Sözleşmesi" kapsamında yayınlanır.
-# Lütfen bkz. < https://github.com/aylak-github/CallTone/blob/master/LICENSE >
-#
-# Her hakkı saklıdır.
-#
-# ========================================================================
-
-import os
-import random
-
-from pyrogram import Client, filters
-from pyrogram.enums import *
-from pyrogram.types import Message
 
 import io
 
