ffd0cf81bcefd2dfd054de196d0e6a5d0931c760
Ali <104919540+olurunabirak@users.noreply.github.com>
2025-01-27 17:32:13 +0300

    Update atag.py

--- ArchMusic/plugins/play/atag.py
+++ ArchMusic/plugins/play/atag.py
@@ -38,7 +38,7 @@ from strings import get_command
 from ArchMusic import app
 from ArchMusic.utils.database import set_cmode
 from ArchMusic.utils.decorators.admins import AdminActual
-from strings import get_command,jokes
+from strings import get_command
 
 
 commandList = [
