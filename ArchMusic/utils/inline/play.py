ba8c9fad842d052b8644646ea6d129f4fab97509
Ali <104919540+olurunabirak@users.noreply.github.com>
2025-02-07 21:10:27 +0300

    Update play.py

--- ArchMusic/utils/inline/play.py
+++ ArchMusic/utils/inline/play.py
@@ -42,15 +42,15 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
     
 
-        [
-            "  <blockquote>▣  InlineKeyboardButton(text="✚ ᴘᴀʀᴄ̧ᴀʏɪ ᴛᴇᴋʀᴀʀʟᴀ ✚", callback_data=f"ADMIN Loop|{chat_id}"),
+        [ """  <blockquote>▣
+              InlineKeyboardButton(text="✚ ᴘᴀʀᴄ̧ᴀʏɪ ᴛᴇᴋʀᴀʀʟᴀ ✚", callback_data=f"ADMIN Loop|{chat_id}"),
         ],                 
         [
             InlineKeyboardButton(text="▷",callback_data=f"ADMIN Resume|{chat_id}",),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="‣‣", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-           <blockquote>▣  "
+           <blockquote>▣  """
         ],
         
         [
