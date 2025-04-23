5057499cc137a1e1c189fc51a6897212df28a7a8
Ali <104919540+deliyim3434@users.noreply.github.com>
2025-04-23 19:50:21 +0300

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -13,84 +13,7 @@ HELP_1 = """ <u>**ETİKET KOMUTU:**</u>
 🔹 /stag Seçtiğiniz birisini seçtiğiniz sticker ile etiketler. Gizli Etiket😶‍🌫️
 🔹 /itag Seçtiğiniz birisini rastegele bir emoji ile etiketleyin 
 """
-
-
-
-HELP_2 = """💥<u>**DEGİSTİRME KOMUTLARI:**</u>
-
-⏸️ /duraklat: Müziği duraklatır.
-⏯️ /devam: Müziği devam ettirir.
-⏩ /atla: Çalan müziği atlar.
-⏹️ /dur: Müziği durdurur.
-🔀 /karistir: Parçaları karıştırır.
-📑 /sira: Sıradaki müzikleri gösterir
-⏭️ /ilerisar: Müziği ileri sarar (Ör: /ilerisar 15).
-⏮️ /gerisar: Müziği geri sarar (Ör: /gerisar 15).
-🔁 /dongu: Müziği döngüye alır (Ör: /dongu 3).
- 
-
-"""
-HELP_3 = """🥏<u>**💥OYNATMA KOMUTU
-▶️ /oynat /play: Müziği oynatır.
--Örnek: /oynat /play şarkı ismi (Ör: /oynat Gönül Dağı).
-
-🎦 /vplay /voynat: Videoyu oynatır.
--Örnek: /vplay /vplay video ismi.
-
-"""
-HELP_4= """🥏<u>** ✨Oynatma Listeleri:
-✧ /yetkiver [Kullanıcı adı] - Kullanıcıyı grubun YETKİLİ listesine ekleyin.
-✧ /yetkial [Kullanıcı adı] - Kullanıcıyı grubun YETKİLİ listesinden çıkarın.
-✧ /yetkililer - Grubun YETKİLİ listesini kontrol edin.
-❤️‍🩹 /playlist: Oynatma listenizi gösterir.
-🆑 /listemisil: Listeden müzik siler.
-📃 /oynat: Oynatma listesi ekranı açar
-"""
-HELP_5= """🥏<u>**MUZİK İNDİRME KOMUTU**</u>
-
-🗨️ /sarkisozu [müzik adı]: Şarkı sözlerini arar.
-📥 /bul /indir [müzik adı] veya [Youtube linki]: Youtube'dan müzik indirir.
-
-"""
-HELP_6=  """🥏<u>**GRUP ENGELLE BLACKLIST CHAT FUNCTION </u>**
-
-/blacklistchat veya /engelle [CHAT_ID] - Music Bot'u kullanarak yapılan tüm sohbetleri kara listeye alın
-/whitelistchat [CHAT_ID] - Kara listeye alınmış herhangi bir sohbeti Music Bot kullanarak beyaz listeye alın
-/blacklistedchat veya /engelliler- Kara listeye alınan tüm sohbetleri kontrol edin.
-/gban [Kullanıcı adı veya kişisel yanıt] - Bir kullanıcıyı botun sunduğu sohbetten yasaklayın ve onun botunuzu kullanmasını engelleyin.
- /ungban [Kullanıcı adı veya kullanıcıya yanıt] - Bir kullanıcıyı Bot'un yasaklı listesinden kaldırın ve onun botunuzu kullanmasına izin verin
-/gbannedusers - G Yasaklı Kullanıcı Listelerini Kontrol Edin
-  """
-HELP_7= """🥏<u>**REKLAM KOMUTLARI BROADCAST FUNCTION </u>**
-/broadcast [Mesaj Gönderme veya Mesaja Cevap Verme] - Herhangi bir mesajı Bot'un Sunulan Sohbetlerine yayınlayın.
-╰☞**-pin** : Bu, mesajınızı sabitleyecektir
-╰☞**-pinloud** : Bu, mesajınızı yüksek sesli bildirimle sabitleyecektir
- ╰☞**-user** : Bu, mesajınızı botunuzu başlatan kullanıcılara yayınlayacaktır.
- ╰☞**-assistant** :Bu, mesajınızı botunuzun asistan hesabından yayınlayacaktır .
-  ╰☞**-nobot** : Bu, botunuzu mesaj yayınlamamaya zorlayacaktır
- **Example:╰☞** `/broadcast -user -assistant -pin Merhaba Testi
-"""
-
-HELP_8=  """🥏<u>**BOT KOMUTLARİ
- /start: Başlatma panelini gösterir.
-⚙️ /ayarlar: Ayarlar menüsüne ulaşır.
-🆘 /yardim: Yardım menüsüne ulaşır.
-📡 /ping: Ping süresini ve sistem durumunu gösterir.
-🖲️/restart :Botu Yeniden Başlatır.
-/reboot - Botunuzu yeniden başlatın.
- /update - Botu Güncelle.
-/speedtest - Sunucu hızlarını kontrol edin
-/maintenance [enable / disable]  Botu BAkima alir
- /logger [enable / disable] - Bot, aranan sorguları günlükçü grubunda günlüğe kaydeder.
- /get_log [Number of Lines] - Heroku veya vps'den botunuzun kaydını alın. her ikisi için de çalışır.
-/autoend [enable|disable] - Enable Kimse dinlemiyorsa otomatik yayın 3 dakika sonra sona erer.
-/addsudo [Username or Reply to a user] Sudo ekler
-/delsudo [Username or Reply to a user] sudo listesinde cikarir
- 🆘 /sudolist: Sudo kullanıcılarına ulaşır.
-"""
-
-
-HELP_9  """ <u>**Eğlence komutları:**</u>
+HELP_2 = """ <u>**Eğlence komutları:**</u>
 🎲 Eğlence komutlarım:
 yapay zeka 
 
@@ -126,3 +49,80 @@ kapat /chatmode kapat
 
 🔹 /barcode - Bir metni barkoduna dönüştürün. 📱(EAN13)
 """
+
+
+
+
+HELP_3 = """💥<u>**DEGİSTİRME KOMUTLARI:**</u>
+
+⏸️ /duraklat: Müziği duraklatır.
+⏯️ /devam: Müziği devam ettirir.
+⏩ /atla: Çalan müziği atlar.
+⏹️ /dur: Müziği durdurur.
+🔀 /karistir: Parçaları karıştırır.
+📑 /sira: Sıradaki müzikleri gösterir
+⏭️ /ilerisar: Müziği ileri sarar (Ör: /ilerisar 15).
+⏮️ /gerisar: Müziği geri sarar (Ör: /gerisar 15).
+🔁 /dongu: Müziği döngüye alır (Ör: /dongu 3).
+ 
+
+"""
+HELP_4 = """🥏<u>**💥OYNATMA KOMUTU
+▶️ /oynat /play: Müziği oynatır.
+-Örnek: /oynat /play şarkı ismi (Ör: /oynat Gönül Dağı).
+
+🎦 /vplay /voynat: Videoyu oynatır.
+-Örnek: /vplay /vplay video ismi.
+
+"""
+HELP_5= """🥏<u>** ✨Oynatma Listeleri:
+
+❤️‍🩹 /playlist: Oynatma listenizi gösterir.
+🆑 /listemisil: Listeden müzik siler.
+📃 /oynat: Oynatma listesi ekranı açar
+"""
+
+HELP_6= """🥏<u>**MUZİK İNDİRME KOMUTU**</u>
+
+🗨️ /sarkisozu [müzik adı]: Şarkı sözlerini arar.
+📥 /bul /indir [müzik adı] veya [Youtube linki]: Youtube'dan müzik indirir.
+
+"""
+HELP_7=  """🥏<u>**GRUP ENGELLE BLACKLIST CHAT FUNCTION </u>**
+
+/blacklistchat veya /engelle [CHAT_ID] - Music Bot'u kullanarak yapılan tüm sohbetleri kara listeye alın
+/whitelistchat [CHAT_ID] - Kara listeye alınmış herhangi bir sohbeti Music Bot kullanarak beyaz listeye alın
+/blacklistedchat veya /engelliler- Kara listeye alınan tüm sohbetleri kontrol edin.
+/gban [Kullanıcı adı veya kişisel yanıt] - Bir kullanıcıyı botun sunduğu sohbetten yasaklayın ve onun botunuzu kullanmasını engelleyin.
+ /ungban [Kullanıcı adı veya kullanıcıya yanıt] - Bir kullanıcıyı Bot'un yasaklı listesinden kaldırın ve onun botunuzu kullanmasına izin verin
+/gbannedusers - G Yasaklı Kullanıcı Listelerini Kontrol Edin
+  """
+HELP_8= """🥏<u>**REKLAM KOMUTLARI BROADCAST FUNCTION </u>**
+/broadcast [Mesaj Gönderme veya Mesaja Cevap Verme] - Herhangi bir mesajı Bot'un Sunulan Sohbetlerine yayınlayın.
+╰☞**-pin** : Bu, mesajınızı sabitleyecektir
+╰☞**-pinloud** : Bu, mesajınızı yüksek sesli bildirimle sabitleyecektir
+ ╰☞**-user** : Bu, mesajınızı botunuzu başlatan kullanıcılara yayınlayacaktır.
+ ╰☞**-assistant** :Bu, mesajınızı botunuzun asistan hesabından yayınlayacaktır .
+  ╰☞**-nobot** : Bu, botunuzu mesaj yayınlamamaya zorlayacaktır
+ **Example:╰☞** `/broadcast -user -assistant -pin Merhaba Testi
+"""
+
+HELP_9=  """🥏<u>**BOT KOMUTLARİ
+ /start: Başlatma panelini gösterir.
+⚙️ /ayarlar: Ayarlar menüsüne ulaşır.
+🆘 /yardim: Yardım menüsüne ulaşır.
+📡 /ping: Ping süresini ve sistem durumunu gösterir.
+🖲️/restart :Botu Yeniden Başlatır.
+/reboot - Botunuzu yeniden başlatın.
+ /update - Botu Güncelle.
+/speedtest - Sunucu hızlarını kontrol edin
+/maintenance [enable / disable]  Botu BAkima alir
+ /logger [enable / disable] - Bot, aranan sorguları günlükçü grubunda günlüğe kaydeder.
+ /get_log [Number of Lines] - Heroku veya vps'den botunuzun kaydını alın. her ikisi için de çalışır.
+/autoend [enable|disable] - Enable Kimse dinlemiyorsa otomatik yayın 3 dakika sonra sona erer.
+/addsudo [Username or Reply to a user] Sudo ekler
+/delsudo [Username or Reply to a user] sudo listesinde cikarir
+ 🆘 /sudolist: Sudo kullanıcılarına ulaşır.
+"""
+
+
