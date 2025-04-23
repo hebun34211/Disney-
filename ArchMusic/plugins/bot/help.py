b251f56c08881e76992da5bd744b1e78e82085ea
Ali <104919540+deliyim3434@users.noreply.github.com>
2025-04-23 17:40:49 +0300

    Update help.py

--- ArchMusic/plugins/bot/help.py
+++ ArchMusic/plugins/bot/help.py
@@ -139,3 +139,17 @@ async def helper_cb(client, CallbackQuery, _):
             helpers.HELP_9, reply_markup=keyboard
 
         )
+      elif cb == "hb10":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_10, reply_markup=keyboard
+
+        )
+
+
+elif cb == "hb11":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_11, reply_markup=keyboard
+
+        )
+
+
